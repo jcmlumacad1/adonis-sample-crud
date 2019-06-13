694f28adc5b938166da6e93a6b159c39120d4889
jcmlumacad1 <jcmlumacad@tmj.ph>
2019-06-13 14:26:59 +0800

    changed schema

--- database/migrations/1560405211665_user_schema.js
+++ database/migrations/1560405211665_user_schema.js
@@ -9,11 +9,11 @@ class UserSchema extends Schema {
       table.charset('utf8')
       table.increments()
       table.string('employee_id').unique().notNullable()
-      .references('id').inTable('departments')
       table.string('first_name')
       table.string('last_name')
       table.string('position_title')
       table.integer('department_id').unsigned()
+      .references('id').inTable('departments')
       table.timestamps()
     })
   }
