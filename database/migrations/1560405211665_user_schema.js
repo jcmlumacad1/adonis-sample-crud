ea016b93611e24fb7f47b952d49ecdc67d8dab86
jcmlumacad1 <jcmlumacad@tmj.ph>
2019-06-13 14:20:40 +0800

    added user schema

--- database/migrations/1560405211665_user_schema.js
+++ database/migrations/1560405211665_user_schema.js
@@ -6,7 +6,14 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
+      table.charset('utf8')
       table.increments()
+      table.string('employee_id').unique().notNullable()
+      .references('id').inTable('departments')
+      table.string('first_name')
+      table.string('last_name')
+      table.string('position_title')
+      table.integer('department_id').unsigned()
       table.timestamps()
     })
   }
